ef7885aa17af2469e791d2e76c19a275ee139183
mduberstein <mduberstein@gmail.com>
2021-11-28 17:43:24 -0500

    In Module 6: After: Clip "Resolve Static Files". End of Module 6.

--- src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -3,10 +3,12 @@ package com.pluralsight.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
@@ -15,5 +17,12 @@ public class ConferenceConfig {
         bean.setSuffix(".jsp");
         bean.setOrder(0);           // a must, in case multiple ViewResolvers are defined
         return bean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        WebMvcConfigurer.super.addResourceHandlers(registry); // auto generated code
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
     }
 }
