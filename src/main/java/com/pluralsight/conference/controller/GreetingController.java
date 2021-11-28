4c2ef719d04bfe3388638aa4bfea9e08a342e84a
mduberstein <mduberstein@gmail.com>
2021-11-28 11:04:32 -0500

    Before Module 5: "Demo Registration Controller"

---
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.pluralsight.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @GetMapping("greeting")
+    public String greeting (Map<String, Object> model) {
+        // will display "Hello Bryan" instead of ${message} in the greeting.jsp page
+        model.put("message", "Hello from Greetings Controller, Bryan");
+        // this will cause greeting.jsp page to be displayed
+        return "greeting";
+    }
+}
